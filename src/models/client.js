537f1d71631bba28c31b8b094412bf5fca9baa55
Robson <14117524+pspr@users.noreply.github.com>
2021-08-31 21:31:39 -0300

    add a post on method client

--- src/models/client.js
+++ src/models/client.js
@@ -3,8 +3,7 @@ const { uuid } = require('uuidv4');
 const dbConnection = require('../services/database');
 
 const ClientSchema = new mongoose.Schema({
-    id: { type: String, required: Boolean },
-    Name: { type: String, required: Boolean },
+    name: { type: String, required: Boolean },
 	phone: { type: String, required: Boolean }
 });
 
